7b66d1992ddd9ae12443a4b5300fbc87cece63f7
volantWizard <122047976@qq.com>
2017-10-09 17:18:46 +0800

    add files

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,77 @@
+var path = require('path')
+var utils = require('./utils')
+var config = require('../config')
+var vueLoaderConfig = require('./vue-loader.conf')
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  entry: {
+    app: './src/main.js'
+  },
+  output: {
+    path: config.build.assetsRoot,
+    filename: '[name].js',
+    publicPath: process.env.NODE_ENV === 'production'
+      ? config.build.assetsPublicPath
+      : config.dev.assetsPublicPath
+  },
+  resolve: {//解析请求(require,import)的模块选项配置
+    extensions: ['.js', '.vue', '.json'],//扩展名
+    alias: {//模块路径别名
+      'src': resolve('src'),
+      'common': resolve('src/common'),
+      'components': resolve('src/components'),
+      'api': resolve('src/api')
+    }
+  },
+  module: {//加载loader模块配置
+    rules: [
+      {//eslint 预检查
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: [resolve('src'), resolve('test')],
+        options: {
+          formatter: require('eslint-friendly-formatter')
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: vueLoaderConfig
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src'), resolve('test')]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  }
+}
---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="recommend">
+    <div class="slider-wrapper">
+
+    </div>
+    <div class="recommend-list">
+
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import {getRecommend} from 'api/recommend'
+  import {ERR_OK} from 'api/config'
+
+  export default {
+    data() {
+      return {
+        sliderList: []
+      }
+    },
+    created() {
+      this._getRecommend()
+    },
+    methods: {
+      _getRecommend() {
+        console.log(1)
+        getRecommend().then((res) => {
+          console.log(res)
+          if (res.status === ERR_OK) {
+            this.sliderList = res.data.slider
+          }
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
